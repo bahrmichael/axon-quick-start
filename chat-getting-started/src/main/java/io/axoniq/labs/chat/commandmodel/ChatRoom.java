0aa0618b7b8041e7e984d02ad6d5be5bd3c87472
Michael Bahr <michael.bahr@valtech.de>
2018-09-22 22:09:14 +0200

    implemented handlers to get the tests green

--- chat-getting-started/src/main/java/io/axoniq/labs/chat/commandmodel/ChatRoom.java
+++ chat-getting-started/src/main/java/io/axoniq/labs/chat/commandmodel/ChatRoom.java
@@ -1,6 +1,12 @@
 package io.axoniq.labs.chat.commandmodel;
 
 import io.axoniq.labs.chat.coreapi.CreateRoomCommand;
+import io.axoniq.labs.chat.coreapi.JoinRoomCommand;
+import io.axoniq.labs.chat.coreapi.LeaveRoomCommand;
+import io.axoniq.labs.chat.coreapi.MessagePostedEvent;
+import io.axoniq.labs.chat.coreapi.ParticipantJoinedRoomEvent;
+import io.axoniq.labs.chat.coreapi.ParticipantLeftRoomEvent;
+import io.axoniq.labs.chat.coreapi.PostMessageCommand;
 import io.axoniq.labs.chat.coreapi.RoomCreatedEvent;
 import org.axonframework.commandhandling.CommandHandler;
 import org.axonframework.commandhandling.model.AggregateIdentifier;
@@ -8,11 +14,16 @@ import org.axonframework.commandhandling.model.AggregateLifecycle;
 import org.axonframework.eventsourcing.EventSourcingHandler;
 import org.axonframework.spring.stereotype.Aggregate;
 
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+
 @Aggregate
 public class ChatRoom {
 
     @AggregateIdentifier
     private String roomId;
+
+    private List<String> participantIds = new CopyOnWriteArrayList<>();
 
     public ChatRoom() {
     }
@@ -22,11 +33,43 @@ public class ChatRoom {
         AggregateLifecycle.apply(new RoomCreatedEvent(cmd.getRoomId(), cmd.getName()));
     }
 
+    @CommandHandler
+    public void on(JoinRoomCommand cmd) {
+        if (!participantIds.contains(cmd.getParticipant())) {
+            AggregateLifecycle.apply(new ParticipantJoinedRoomEvent(cmd.getParticipant(), cmd.getRoomId()));
+        }
+    }
+
+    @CommandHandler
+    public void on(LeaveRoomCommand cmd) {
+        if (participantIds.contains(cmd.getParticipant())) {
+            AggregateLifecycle.apply(new ParticipantLeftRoomEvent(cmd.getParticipant(), cmd.getRoomId()));
+        }
+    }
+
+    @CommandHandler
+    public void on(PostMessageCommand cmd) {
+        if (participantIds.contains(cmd.getParticipant())) {
+            AggregateLifecycle.apply(new MessagePostedEvent(cmd.getParticipant(), cmd.getRoomId(), cmd.getMessage()));
+        } else {
+            throw new IllegalStateException("Participant " + cmd.getParticipant() + "has not joined the room " + roomId);
+        }
+    }
+
     @EventSourcingHandler
     public void on(RoomCreatedEvent evt) {
         roomId = evt.getRoomId();
     }
 
-    // TODO: This class has just been created to make the test compile. It's missing, well, everything...
+    @EventSourcingHandler
+    public void on(ParticipantJoinedRoomEvent evt) {
+        // todo: should this check for a matching roomId or will we only receive proper roomIds due to the aggregateIdentifier?
+        this.participantIds.add(evt.getParticipant());
+    }
+
+    @EventSourcingHandler
+    public void on(ParticipantLeftRoomEvent evt) {
+        this.participantIds.remove(evt.getParticipant());
+    }
 
 }
