edecd655d801208e12e5c024291f7e7d5437df95
Michael Bahr <michael.bahr@valtech.de>
2018-09-22 19:03:04 +0200

    Equipped ChatRoom with command and event handlers

--- chat-getting-started/src/main/java/io/axoniq/labs/chat/commandmodel/ChatRoom.java
+++ chat-getting-started/src/main/java/io/axoniq/labs/chat/commandmodel/ChatRoom.java
@@ -1,6 +1,31 @@
 package io.axoniq.labs.chat.commandmodel;
 
+import io.axoniq.labs.chat.coreapi.CreateRoomCommand;
+import io.axoniq.labs.chat.coreapi.RoomCreatedEvent;
+import org.axonframework.commandhandling.CommandHandler;
+import org.axonframework.commandhandling.model.AggregateIdentifier;
+import org.axonframework.commandhandling.model.AggregateLifecycle;
+import org.axonframework.eventsourcing.EventSourcingHandler;
+import org.axonframework.spring.stereotype.Aggregate;
+
+@Aggregate
 public class ChatRoom {
+
+    @AggregateIdentifier
+    private String roomId;
+
+    public ChatRoom() {
+    }
+
+    @CommandHandler
+    public ChatRoom(CreateRoomCommand cmd) {
+        AggregateLifecycle.apply(new RoomCreatedEvent(cmd.getRoomId(), cmd.getName()));
+    }
+
+    @EventSourcingHandler
+    public void on(RoomCreatedEvent evt) {
+        roomId = evt.getRoomId();
+    }
 
     // TODO: This class has just been created to make the test compile. It's missing, well, everything...
 
